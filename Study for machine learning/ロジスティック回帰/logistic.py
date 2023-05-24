58e0b792282838c02a1db69b58c380829c10a01e
mksmkss <masataka2003@gmail.com>
2023-05-24 16:48:07 +0900

    2023/5/24-1 Suzuki

--- Study for machine learning/ロジスティック回帰/logistic.py
+++ Study for machine learning/ロジスティック回帰/logistic.py
@@ -1,7 +1,8 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split　
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc, confusion_matrix
 
 """
 https://qiita.com/0NE_shoT_/items/b702ab482466df6e5569
@@ -13,6 +14,8 @@ petal_width	    花弁の幅
 species	        品種
 
 https://best-biostatistics.com/correlation_regression/logistic.html
+グラフに関して
+https://python.atelierkobato.com/logistic/
 """
 
 iris_df = sns.load_dataset("iris")  # データセットの読み込み
@@ -36,5 +39,20 @@ X_train, X_test, Y_train, Y_test = train_test_split(
 
 lr = LogisticRegression()  # ロジスティック回帰モデルのインスタンスを作成
 lr.fit(X_train, Y_train)  # ロジスティック回帰モデルの重みを学習
+# 結果を出力
+print("傾き = ", lr.coef_[0][0])
+print("切片 = ", lr.intercept_[0])
 
-print("切片")
+Y_score = lr.predict_proba(X_test)[:, 1]  # 検証データがクラス1に属する確率
+Y_pred = lr.predict(X_test)  # 検証データのクラスを予測
+fpr, tpr, thresholds = roc_curve(y_true=Y_test, y_score=Y_score)
+
+
+print("confusion matrix = \n", confusion_matrix(y_true=Y_test, y_pred=Y_pred))
+# plt.plot(fpr, tpr, label="roc curve (area = %0.3f)" % auc(fpr, tpr))
+# plt.plot([0, 1], [0, 1], linestyle="--", label="random")
+# plt.plot([0, 0, 1], [0, 1, 1], linestyle="--", label="ideal")
+# plt.legend()
+# plt.xlabel("false positive rate")
+# plt.ylabel("true positive rate")
+# plt.show()
